416d0c2b0bd98f7010f078da6c5a1d0413f59111
Abdul Mannan <mannanmaan1425@gmail.com>
2024-09-25 05:11:46 +0500

    new

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -5,4 +5,17 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    type = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
